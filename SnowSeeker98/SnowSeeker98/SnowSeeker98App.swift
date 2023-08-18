d04dac02ba0237788b7dd4f5a4913ead69427003
Sandra2506 <110335985+Sandra2506@users.noreply.github.com>
2023-08-18 10:45:10 +0300

    Add files via upload

---
+++ SnowSeeker98/SnowSeeker98/SnowSeeker98App.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeeker98App.swift
+//  SnowSeeker98
+//
+//  Created by Александра Фонова on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeeker98App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
